df483f809e9017516b1d117e292a217d2311e5bc
pardipYadav <213sunilyadav@gmail.com>
2025-05-06 05:52:06 +0530

    todo list crud

--- excercise_7/todo.js
+++ excercise_7/todo.js
@@ -55,7 +55,7 @@ function addTableRow(value) {
   let actionCell = document.createElement("td");
   SnCell.textContent = snCount++;
   nameCell.textContent = value;
-  actionCell.innerHTML = `<a href="#" onclick="complete(this)">Complete</a>| <a href="">Edit</a> | <a href="">Delete</a>`;
+  actionCell.innerHTML = `<a href="#" onclick="complete(this)">Complete</a>| <a href="#" onclick="editTask(this)">Edit</a> | <a href="#" onclick="deleteTask(this)">Delete</a>`;
   row.appendChild(SnCell);
   row.appendChild(nameCell);
   row.appendChild(actionCell);
@@ -69,25 +69,84 @@ window.onload = function () {
     data.split(",").forEach(addTableRow);
   }
 };
-function complete(clickact) {
-  let row = clickact.closest("tr");
-  let cell = row.getElementsByTagName("td");
-  let name = cell[1].textContent;
-  actionPerform(name);
+function complete(currentVal) {
+  actionPerform(currentVal, "complete");
 }
+function editTask(currentVal) {
+  actionPerform(currentVal, "edit");
+}
+function deleteTask(currentVal) {
+  actionPerform(currentVal, "delete");
+}
+function actionPerform(val, action) {
+  if (action === "complete") {
+    let row = val.closest("tr");
+    let cell = row.getElementsByTagName("td");
+    let targetCell = cell[1];
+    let originalText = targetCell.textContent;
+    targetCell.innerHTML = `<span style="text-decoration:line-through">${originalText}</span>`;
+  } else if (action === "edit") {
+    let row = val.closest("tr");
+    let cell = row.getElementsByTagName("td");
+    let name = cell[1].textContent;
+    let editpromptBox = prompt("Please edit your task", `${name}`);
+    let tasklocalstg = localStorage.getItem("task");
+    let valueOf = tasklocalstg.split(",");
+    // let found = valueOf.find((item) => {
+    //   console.log(item);
+    //   item !== editpromptBox;
+    // });
+    let found = valueOf.indexOf(name);
+    if (found === -1) {
+      alert("the previous value is not match in existing localstorage data");
+      return;
+    }
+    valueOf[found] = editpromptBox;
+    localStorage.setItem("task", valueOf.join(","));
 
-function actionPerform(fetchData) {
-  let storedData = localStorage.getItem("task");
-  if (!storedData) return;
-
-  let data = storedData.split(",");
-  console.log(data);
-  if (data.includes(fetchData)) {
-    let upData = data.filter((item) => item !== fetchData);
-
-    localStorage.setItem("task", d);
-    console.log("remove successfully");
-  } else {
-    console.log("something went wrong");
+    // addTableRow(editpromptBox);
+  } else if (action === "delete") {
+    let row = val.closest("tr");
+    let cell = row.getElementsByTagName("td");
+    let name = cell[1].textContent;
+    let getData = localStorage.getItem("task");
+    let data = getData.split(",");
+    // let compare = data.filter((item) => {
+    //   item !== name;
+    // });
+    // console.log(data);
+    let compareData = data.indexOf(name);
+    if (compareData === -1) {
+      alert("Value not found.");
+      return;
+    }
+    let values = data.splice(compareData, 1);
+    console.log(values);
+    localStorage.setItem("task", values.join(","));
+    console.log("Updated localStorage:", values.join(","));
   }
 }
+
+// function actionPerform(fetchData, actionName) {
+//   if (actionName === "complete") {
+//     let storedData = localStorage.getItem("task");
+//     if (!storedData) return;
+//     let data = storedData.split(",");
+//     if (data.includes(fetchData)) {
+//       const table = document.querySelector("table");
+//       const cell = table.querySelectorAll("td");
+//       const targetCellIndex = 2;
+//       const targetCell = cell[targetCellIndex];
+//       const originalText = targetCell.textContent;
+//       targetCell.innerHTML = `<span style="text-decoration:line-through">${originalText}</span>`;
+//       //   console.log(originalText);
+
+//       //   let upData = data.filter((item) => item !== fetchData);
+
+//       //   localStorage.setItem("task", upData);
+//       //   console.log("remove successfully");
+//     } else {
+//       console.log("something went wrong");
+//     }
+//   }
+// }
